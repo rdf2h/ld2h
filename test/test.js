ab7b251f66853a3054eb3adefc763665c7a0a971
bergi <bergi@axolotlfarm.org>
2016-07-01 22:29:11 +0200

    replaced tape tests with mocha + phantomjs tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,27 @@
-import test from 'tape'
-import LD2h from '../'
-import report from 'browserify-tape-spec'
+/* global describe, it */
 
-test('exists', (t)=> {
-  t.ok(test, 'test lib correctly imported')
-  t.end()
+require('es6-promise').polyfill()
+
+var assert = require('assert')
+var n3Parser = require('rdf-parser-n3')
+var rdf = require('rdf-ext')
+var LD2h = require('..')
+
+rdf.parsers['text/turtle'] = n3Parser
+
+describe('LD2h', function () {
+  it('should be a function', function () {
+    assert.equal(typeof LD2h, 'function')
+  })
+
+  it('.store should be a LdpStore', function () {
+    assert.equal(typeof LD2h.store, 'object')
+    assert.equal(typeof LD2h.store.parsers, 'object')
+  })
+
+  it('.getDataGraph should the graph data embedded in the #data element', function () {
+    return LD2h.getDataGraph().then(function (graph) {
+      assert.equal(graph.match(null, 'http://schema.org/headline', 'LD2h Tests').length, 1)
+    })
+  })
 })
-
-test('LD2h exists', (t)=> {
-  t.ok(LD2h, 'echo lib correctly imported')
-  t.end()
-})
-
-if (process.browser) test.createStream().pipe(report('out'))
